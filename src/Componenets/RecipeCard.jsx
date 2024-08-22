2535b63b8a3838fc105985683e8db9dad0f36f02
Grace Clower <clower.grace@gmail.com>
2024-08-22 19:30:22 -0400

    working on getting the modal pop up with all the relevant information. confirmed that video and source buttons only appear when the recipe has one of those elements

--- src/Componenets/RecipeCard.jsx
+++ src/Componenets/RecipeCard.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './RecipeCard.css'
 import { useState } from 'react'
 import Modal from "react-modal"
+import { Link } from 'react-router-dom'
 
 function RecipeCard({recipe}) {
     const [modalIsOpen, setIsOpen] = useState(false)
@@ -25,10 +26,28 @@ function RecipeCard({recipe}) {
         <Modal
         isOpen={modalIsOpen}
         className="recipe-modal-popup">
+            <img className="recipe-modal-image" src={recipe.image} alt="image of recipe"></img>
             <h1 className='recipe-modal-title'>{recipe.name}</h1>
+            <h3>Ingredients</h3>
+            <ul className='recipe-modal-ingredients'>
+                {recipe.ingredients.map((ingredient)=>(
+                    <li className='recipe-ingredient'>{ingredient}</li>
+                ))}
+            </ul>
+            <p className='recipe-instructions'>{recipe.instructions}</p>
+            {recipe.video &&
+               <button className="recipe-modal-video-button" onClick= {()=> window.open(recipe.video, '_blank')}>Video Walkthrough</button>
+            }
+                        {recipe.source &&
+               <button className="recipe-modal-source-button" onClick= {()=> window.open(recipe.source, '_blank')}>Source</button>
+            }
+            <button onClick={closeModal}>X</button>
+            
         </Modal>
+
         <div className='recipe-info'>
             <h1 className='recipe-name'>{recipe.name}</h1>
+            
         </div>
     </div>
   )
