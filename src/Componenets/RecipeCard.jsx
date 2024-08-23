fc25dc971739a9572a1c7f35809549d69fbb1344
Grace Clower <clower.grace@gmail.com>
2024-08-22 20:07:44 -0400

    finished with basic formatting of the modal popup. close button and video and source buttons formatted. next to work on the filter search on main page

--- src/Componenets/RecipeCard.jsx
+++ src/Componenets/RecipeCard.jsx
@@ -1,56 +1,75 @@
-import React from 'react'
-import './RecipeCard.css'
-import { useState } from 'react'
-import Modal from "react-modal"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./RecipeCard.css";
+import { useState } from "react";
+import Modal from "react-modal";
+import { Link } from "react-router-dom";
 
-function RecipeCard({recipe}) {
-    const [modalIsOpen, setIsOpen] = useState(false)
+function RecipeCard({ recipe }) {
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-    function openModal(){
-        setIsOpen(true)
-    }
-    function closeModal(){
-        setIsOpen(false)
-      }
-    
-    const backgroundImage = {
-        backgroundImage: `url(${recipe.image})`
-    }
+  function openModal() {
+    setIsOpen(true);
+  }
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const backgroundImage = {
+    backgroundImage: `url(${recipe.image})`,
+  };
 
   return (
-    <div 
-    className='recipe-card'
-    onClick={modalIsOpen ? closeModal : openModal}
-    style={backgroundImage}>
-        <Modal
-        isOpen={modalIsOpen}
-        className="recipe-modal-popup">
-            <img className="recipe-modal-image" src={recipe.image} alt="image of recipe"></img>
-            <h1 className='recipe-modal-title'>{recipe.name}</h1>
-            <h3>Ingredients</h3>
-            <ul className='recipe-modal-ingredients'>
-                {recipe.ingredients.map((ingredient)=>(
-                    <li className='recipe-ingredient'>{ingredient}</li>
-                ))}
-            </ul>
-            <p className='recipe-instructions'>{recipe.instructions}</p>
-            {recipe.video &&
-               <button className="recipe-modal-video-button" onClick= {()=> window.open(recipe.video, '_blank')}>Video Walkthrough</button>
-            }
-                        {recipe.source &&
-               <button className="recipe-modal-source-button" onClick= {()=> window.open(recipe.source, '_blank')}>Source</button>
-            }
-            <button onClick={closeModal}>X</button>
-            
-        </Modal>
-
-        <div className='recipe-info'>
-            <h1 className='recipe-name'>{recipe.name}</h1>
-            
+    <div
+      className="recipe-card"
+      onClick={modalIsOpen ? closeModal : openModal}
+      style={backgroundImage}
+    >
+      <Modal isOpen={modalIsOpen} className="recipe-modal-popup">
+      <button className="recipe-modal-close-button" onClick={closeModal}>X</button>
+        <div className="recipe-modal-intro">
+          <img
+            className="recipe-modal-image"
+            src={recipe.image}
+            alt="image of recipe"
+          ></img>
+          <h1 className="recipe-modal-title">{recipe.name}</h1>
         </div>
+        <div className="recipe-modal-ingredient-section">
+          <h3>Ingredients</h3>
+          <ul className="recipe-modal-ingredients">
+            {recipe.ingredients.map((ingredient) => (
+              <li className="recipe-ingredient">{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+        <h3>Instructions</h3>
+        <p className="recipe-instructions">{recipe.instructions}</p>
+        <div className="recipe-modal-buttons">
+        {recipe.video && (
+          <button
+            className="recipe-modal-video-button"
+            onClick={() => window.open(recipe.video, "_blank")}
+          >
+            Video Walkthrough
+          </button>
+        )}
+        {recipe.source && (
+          <button
+            className="recipe-modal-source-button"
+            onClick={() => window.open(recipe.source, "_blank")}
+          >
+            Source
+          </button>
+        )}
+        </div>
+      
+      </Modal>
+
+      <div className="recipe-info">
+        <h1 className="recipe-name">{recipe.name}</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default RecipeCard
+export default RecipeCard;
